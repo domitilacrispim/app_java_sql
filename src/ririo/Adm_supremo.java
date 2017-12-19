bebeb7148138fa337514eabab1f5136a6bff2143
Unknown <domitilacrispim@gmail.com>
2017-12-19 21:37:58 -0200

    Select testado

--- src/ririo/Adm_supremo.java
+++ src/ririo/Adm_supremo.java
@@ -1,4 +1,3 @@
-
 package ririo;
 
 import java.awt.Color;
@@ -6,6 +5,8 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.GrayFilter;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -15,57 +16,93 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Adm_supremo extends JFrame {
-  //  private String user = "Adm";
- //   private String senha = "123456";
-    public boolean Adm_supremo(String a, String b) throws ClassNotFoundException{
-    //    if ( !a.equals(user) || !b.equals(senha) ) return false ; // subistirui esse if com a conexao com o banco
-        Connection con = null;
-        PreparedStatement pst=null;
+    //  private String user = "Adm";
+    //   private String senha = "123456";
+public Statement sentenca;
+    public boolean Adm_supremo(String a, String b) throws ClassNotFoundException {
+        //    if ( !a.equals(user) || !b.equals(senha) ) return false ; // subistirui esse if com a conexao com o banco
+        final Connection con =  Seguranca.seguranca(a, b);
+        PreparedStatement pst = null;
         ResultSet rs = null;
-        con = Seguranca.seguranca(a,b);
-        if ( con==null ) return false;
-    final ImageIcon imageIcon = new ImageIcon("/home/domitila/Imagens/rir.jpg");
-      JOptionPane.showMessageDialog(null,"Logado com administrador. Instruções "
-                                                   + ""
-                                                   +":\n 1. Você pode apagar, inserir e consultar."
-                                                   +"\n 2. Cuidado ao deletar! Se os campos ficarem em branco TODOS os dados serão excuídos."
-                                                   + "\n 3. Caso você tente deletar informações atreladas a mais de uma tabela a exclusão será feita em cascata. "
-                                                   +"\n 4. Na situção de um dado inserido ser inválido, você será avisado e a inserção não acontecerá."
-                                                   + "\n5. Tenha um bom dia de trabalho! :)");
-            
-   JPanel painel_princ = new JPanel(new FlowLayout()){
-        	Image image = imageIcon.getImage();
-        	Image grayImage = image;
-        	{
-            	setOpaque(false);
-        	}
-        	public void paint(Graphics g) {
-           		g.drawImage(grayImage, -350, -50, this);
-            	super.paint(g);
-        	}
-    	};// painel principal
-     JButton inser = new JButton("Inserção"); // botao de insercao  
-     JButton consult = new JButton("Consulta"); // botao de consulta
-      JButton dele = new JButton("Deleção"); // botao de delecao
-      JTextField tabela = new JTextField(20);
-      JLabel ajuda = new JLabel("Digite o nome da tabela que deseja editar:\n");
-              //painel_princ.setLayout(new GridLayout(4,1));
-               ajuda.setForeground(Color.white);
-        
-              painel_princ.add(ajuda);
-      painel_princ.add(tabela);
-      painel_princ.add(inser);
-      painel_princ.add(dele);
-      painel_princ.add(consult);  
-      JFrame campo = new JFrame("Janela do Adm.");
-        campo.setSize(400, 300);    
+       
+        if (con == null) {
+            return false;
+        }
+        final ImageIcon imageIcon = new ImageIcon("/home/domitila/Imagens/rir.jpg");
+        JOptionPane.showMessageDialog(null, "Logado com administrador. Instruções "
+                + ""
+                + ":\n 1. Você pode apagar, inserir e consultar."
+                + "\n 2. Cuidado ao deletar! Se os campos ficarem em branco TODOS os dados serão excuídos."
+                + "\n 3. Caso você tente deletar informações atreladas a mais de uma tabela a exclusão será feita em cascata. "
+                + "\n 4. Na situção de um dado inserido ser inválido, você será avisado e a inserção não acontecerá."
+                + "\n5. Tenha um bom dia de trabalho! :)");
+
+        JPanel painel_princ = new JPanel(new FlowLayout()) {
+            Image image = imageIcon.getImage();
+            Image grayImage = image;
+
+            {
+                setOpaque(false);
+            }
+
+            public void paint(Graphics g) {
+                g.drawImage(grayImage, -350, -50, this);
+                super.paint(g);
+            }
+        };// painel principal
+        JButton inser = new JButton("Inserção"); // botao de insercao  
+        JButton consult = new JButton("Consulta"); // botao de consulta
+        JButton dele = new JButton("Deleção"); // botao de delecao
+        JTextField tabela = new JTextField(20);
+        JLabel ajuda = new JLabel("Digite o nome da tabela que deseja editar:\n");
+        //painel_princ.setLayout(new GridLayout(4,1));
+        ajuda.setForeground(Color.white);
+
+        painel_princ.add(ajuda);
+        painel_princ.add(tabela);
+        painel_princ.add(inser);
+        painel_princ.add(dele);
+        painel_princ.add(consult);
+        JFrame campo = new JFrame("Janela do Adm.");
+        campo.setSize(400, 300);
         campo.setDefaultCloseOperation(EXIT_ON_CLOSE);
-       campo.getContentPane().add(painel_princ);
+        campo.getContentPane().add(painel_princ);
+        consult.addActionListener(
+                new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+              consulta(tabela.getText(),con );
+                // JOptionPane.showMessageDialog(null, "mimim");
+                
+              
+
+            }
+        }
+        );
         campo.setVisible(true);
-      return true;
+
+        return true;
     }
     
-    
+    public void consulta ( String a, Connection con){
+                   try { // Cria uma sentenca para atualizar o banco de dado
+                       sentenca = con.createStatement();
+                       sentenca.executeUpdate("Set search_path to rockinrio");
+                       sentenca.executeUpdate("Insert into " + a + " values (2, 100, 100, 'mundi', 202)");
+                       JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+                       
+                   } 
+                  
+catch (SQLException se) {
+          JOptionPane.showMessageDialog(null, "Tabela invalida ou voce nao possui esse privilegio!");
+                      
+        System.out.println("Nao foi possivel executar.");
+                           se.printStackTrace();
+        System.exit(1);}
+    }
+
 }
--- src/ririo/operacao.java
+++ src/ririo/operacao.java
@@ -5,10 +5,38 @@
  */
 package ririo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author domitila
  */
 public class operacao {
-    
+    public String Minguado (String tabela,Connection con) throws ClassNotFoundException{
+        try {
+             String consulta = "Select * from " + tabela;
+             PreparedStatement estado;
+             estado = con.prepareStatement(consulta);
+             ResultSet rs =   estado.executeQuery(consulta);
+            String result = new String();
+            while (rs.next()){
+                System.out.print("Column 1 returned ");
+               System.out.println(rs.getString(1));
+            }
+           return result;
+        }
+        catch(SQLException error){
+            JOptionPane.showMessageDialog(null, error);
+            return null;
+        }
+    }
 }
+
+
+  
+
